6724913e0fe20c5efd7fbd8315e1292446f5d154
Alex Arraga <arraga.alex@gmail.com>
2025-09-05 00:02:24 -0300

    init cache service

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"go-ecommerce/internal/adapters/config"
 	"go-ecommerce/internal/adapters/logger"
+	"go-ecommerce/internal/adapters/storage/cache/redis"
 	"go-ecommerce/internal/adapters/storage/database/postgres"
 	"log/slog"
 	"os"
@@ -12,7 +13,7 @@ import (
 func main() {
 	config, err := config.New()
 	if err != nil {
-		slog.Error("error loading environment variables", "error", err)
+		slog.Error("Error loading environment variables", "error", err)
 		os.Exit(1)
 	}
 
@@ -35,5 +36,15 @@ func main() {
 		slog.Error("Error executing database migrations", "error", err)
 		os.Exit(1)
 	}
+	slog.Info("Successfully migrated the database")
+
+	// Init cache service
+	cache, err := redis.New(ctx, config.Redis)
+	if err != nil {
+		slog.Error("Error initializing cache connection", "error", err)
+		os.Exit(1)
+	}
+	defer cache.Close()
+	slog.Info("Successfully connected to the cache server")
 
 }
