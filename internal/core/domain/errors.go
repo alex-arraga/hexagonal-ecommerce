5004ab5046e3caa07537ae4df30a5a96646d4f67
Alex Arraga <arraga.alex@gmail.com>
2025-09-22 11:35:08 -0300

    new errors added

--- internal/core/domain/errors.go
+++ internal/core/domain/errors.go
@@ -3,6 +3,8 @@ package domain
 import "errors"
 
 var (
-	ErrInternal = errors.New("internal error") // ErrInternal is an error for when an internal service fails to process the request
-	ErrConflictingData = errors.New("data conflicts with existing data in unique column") // ErrConflictingData is an error for when data conflicts with existing data
+	ErrInvalidCredentials = errors.New("invalid email or password")                          // ErrInvalidCredentials is an error for when the credentials are invalid
+	ErrDataNotFound       = errors.New("data not found")                                     // ErrDataNotFound is an error for when requested data is not found
+	ErrInternal           = errors.New("internal error")                                     // ErrInternal is an error for when an internal service fails to process the request
+	ErrConflictingData    = errors.New("data conflicts with existing data in unique column") // ErrConflictingData is an error for when data conflicts with existing data
 )
