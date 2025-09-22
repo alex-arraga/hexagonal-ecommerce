da07e0695203288f8058a5b249b07b366b808ed7
Alex Arraga <arraga.alex@gmail.com>
2025-09-22 11:35:26 -0300

    domain hash the user password

--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -15,6 +15,16 @@ const (
 	Seller UserRole = "seller"
 )
 
+// Bussiness rules (constantes)
+const (
+	minPasswordLength = 6
+	minNameLength     = 3
+)
+
+type PasswordHasher interface {
+	Hash(password string) (string, error)
+}
+
 type User struct {
 	ID        uuid.UUID
 	Name      string
@@ -25,14 +35,12 @@ type User struct {
 	UpdatedAt time.Time
 }
 
-// Bussiness rules (constantes)
-const (
-	minPasswordLength = 6
-	minNameLength     = 3
-)
-
 // NewUser creates a new user applying the bussiness rules
-func NewUser(name, email, password string, role UserRole) (*User, error) {
+func NewUser(name, email, password string, role UserRole, hasher PasswordHasher) (*User, error) {
+	if role != Admin && role != Client && role != Seller {
+		return nil, errors.New("role is required")
+	}
+
 	if len(name) < minNameLength {
 		return nil, errors.New("name must have at least 3 characters")
 	}
@@ -45,12 +53,18 @@ func NewUser(name, email, password string, role UserRole) (*User, error) {
 		return nil, errors.New("invalid user role")
 	}
 
+	// hash password with the provided hasher
+	hashedPassword, err := hasher.Hash(password)
+	if err != nil {
+		return nil, errors.New("could not hash password")
+	}
+
 	now := time.Now()
 	return &User{
 		ID:        uuid.New(),
 		Name:      name,
 		Email:     email,
-		Password:  password,
+		Password:  hashedPassword,
 		Role:      role,
 		CreatedAt: now,
 		UpdatedAt: now,
