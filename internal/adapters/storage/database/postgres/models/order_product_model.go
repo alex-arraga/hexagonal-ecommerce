43ed1e4861f60f1dcb1aefd35242e936ab54254a
Alex Arraga <arraga.alex@gmail.com>
2025-09-24 17:45:50 -0300

    order_product model and interface to repository added

---
+++ internal/adapters/storage/database/postgres/models/order_product_model.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type OrderProductModel struct {
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey"`
+	OrderID   uuid.UUID `gorm:"type:uuid;not null"`
+	ProductID uuid.UUID `gorm:"type:uuid;not null"`
+	Quantity  int64     `gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+
+	// Relations
+	Order *OrderModel   `gorm:"foreignKey:OrderID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Items *ProductModel `gorm:"foreignKey:ProductID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+// This function will be executed before to create a new order-product model
+func (op *OrderProductModel) BeforeCreate(tx *gorm.DB) (err error) {
+	if op.ID == uuid.Nil {
+		op.ID = uuid.New()
+	}
+	return
+}
---
+++ internal/core/ports/order_product.go
@@ -0,0 +1,14 @@
+package ports
+
+import (
+	"context"
+	"go-ecommerce/internal/core/domain"
+
+	"github.com/google/uuid"
+)
+
+type OrderProductRepository interface {
+	SaveOrderProduct(ctx context.Context, orderProduct *domain.OrderProduct) (*domain.OrderProduct, error)
+	GetOrderProductById(ctx context.Context, id uuid.UUID) (*domain.OrderProduct, error)
+	ListOrderProducts(ctx context.Context) ([]*domain.OrderProduct, error)
+}
