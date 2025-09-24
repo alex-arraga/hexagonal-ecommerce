9192106f42a27dbb69febc51952ac104ea5349f5
Alex Arraga <arraga.alex@gmail.com>
2025-09-24 17:07:16 -0300

    order and order product entities created

---
+++ internal/core/domain/order.go
@@ -0,0 +1,121 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Providers string
+type Currencies string
+type DisscountTypes string
+type PayStatus string
+type PayStatusDetail string
+
+const (
+	MercadoPago Providers = "mercado-pago"
+)
+
+const (
+	ARS Currencies = "ARS"
+	USD Currencies = "USD"
+)
+
+const (
+	Percentage DisscountTypes = "percentage" // Percentage = subtotal * (15 / 100)
+	Bundle     DisscountTypes = "bundle"     // Bundle = 2x1, 3x2, Pack of 3 services $X
+	Fixed      DisscountTypes = "fixed"      // Fixed = $500
+)
+
+const (
+	Approved    PayStatus = "approved"     //
+	Pending     PayStatus = "pending"      //
+	InProcess   PayStatus = "in_process"   //
+	Authorized  PayStatus = "authorized"   //
+	Cancelled   PayStatus = "cancelled"    //
+	Refunded    PayStatus = "refunded"     //
+	ChargedBack PayStatus = "charged_back" //
+	Rejected    PayStatus = "rejected"     //
+	Expired     PayStatus = "expired"      //
+	SoftDelete  PayStatus = "soft_delete"  //
+	NonExistent PayStatus = "non-existent" //
+)
+
+const (
+	Accredited        PayStatusDetail = "accredited"            //
+	PendingCapture    PayStatusDetail = "pending_capture"       //
+	PartiallyRefunded PayStatusDetail = "partially_refunded"    //
+	InProcessDetail   PayStatusDetail = "in_process"            //
+	ExpiredDetail     PayStatusDetail = "expired"               //
+	BankError         PayStatusDetail = "bank_error"            //
+	Blacklist         PayStatusDetail = "cc_rejected_blacklist" //
+	NonExistentDetail PayStatusDetail = "non-existent"          //
+)
+
+type Order struct {
+	ID                uuid.UUID
+	Providers         Providers
+	UserID            uuid.UUID
+	PaymentID         *string
+	SecureToken       *uuid.UUID // token that allows the user to view the status of the order
+	ExternalReference *string
+	Currency          Currencies
+	SubTotal          float64
+	Disscount         *float64
+	DisscountType     *DisscountTypes
+	Total             float64
+	Paid              bool
+	PayStatus         PayStatus
+	PayStatusDetail   *PayStatusDetail
+	CreatedAt         time.Time
+	UpdatedAt         time.Time
+	ExpiresAt         time.Time
+
+	// Relations
+	User  *User
+	Items []*OrderProduct
+}
+
+type NewOrderInputs struct {
+	UserID         uuid.UUID
+	Currency       Currencies
+	SubTotal       float64
+	Disscount      *float64
+	DisscountTypes *DisscountTypes
+}
+
+func NewOrder(inputs NewOrderInputs) *Order {
+	return &Order{
+		ID:                uuid.Nil, // repository will asign the id
+		UserID:            inputs.UserID,
+		Currency:          inputs.Currency,
+		PayStatus:         Pending,
+		Providers:         MercadoPago,
+		ExternalReference: nil,
+		SecureToken:       nil,
+		PaymentID:         nil,
+		PayStatusDetail:   nil,
+		Paid:              false,
+		SubTotal:          inputs.SubTotal,
+		Disscount:         inputs.Disscount,
+		DisscountType:     inputs.DisscountTypes,
+		Total:             inputs.SubTotal - *inputs.Disscount,
+	}
+}
+
+type UpdateOrderInputs struct {
+	ExternalReference string
+	PaymentID         string
+	PayStatus         PayStatus
+	PayStatusDetail   PayStatusDetail
+}
+
+func (o *Order) UpdateOrder(inputs UpdateOrderInputs) error {
+	o.ExternalReference = &inputs.ExternalReference
+	o.PaymentID = &inputs.PaymentID
+
+	o.PayStatus = inputs.PayStatus
+	o.PayStatusDetail = &inputs.PayStatusDetail
+
+	return nil
+}
---
+++ internal/core/domain/order_product.go
@@ -0,0 +1,30 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// OrderProduct is an entity that represents pivot table between order and product
+type OrderProduct struct {
+	ID        uuid.UUID
+	OrderID   uuid.UUID
+	ProductID uuid.UUID
+	Quantity  int64
+	CreatedAt time.Time
+	UpdatedAt time.Time
+
+	// Relations
+	Order *Order
+	Items *Product
+}
+
+func NewOrderProduct(orderID, productID uuid.UUID, quantity int64) *OrderProduct {
+	return &OrderProduct{
+		ID:        uuid.Nil, // repository will asign the id
+		OrderID:   orderID,
+		ProductID: productID,
+		Quantity:  quantity,
+	}
+}
