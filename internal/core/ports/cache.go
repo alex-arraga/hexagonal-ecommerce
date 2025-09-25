45bea08c8b9cfa386618b3ce1cacc01ee48dd7ab
Alex Arraga <arraga.alex@gmail.com>
2025-09-25 17:54:32 -0300

    delete user cache interface

--- internal/core/ports/cache.go
+++ internal/core/ports/cache.go
@@ -2,7 +2,6 @@ package ports
 
 import (
 	"context"
-	"go-ecommerce/internal/core/domain"
 	"time"
 )
 
@@ -13,12 +12,4 @@ type CacheRepository interface {
 	Delete(ctx context.Context, key string) error                               // Delete removes the value from the cache
 	DeleteByPrefix(ctx context.Context, prefix string) error                    // DeleteByPrefix removes the value from the cache with the given prefix
 	Close() error                                                               // Close closes the connection to the cache server
-}
-
-// UserCacheRepository is an interface for interacting with cache-related user business logic
-type UserCacheRepository interface {
-	GetUser(ctx context.Context, id string) (*domain.User, error)
-	SetUser(ctx context.Context, user *domain.User) error
-	DeleteUser(ctx context.Context, id string) error
-	DeleteAllUsers(ctx context.Context) error
 }
