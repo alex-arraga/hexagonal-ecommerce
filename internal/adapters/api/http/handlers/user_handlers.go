36d5d3af833430c1ccff7fd508a4ed42ec474bf6
Alex Arraga <arraga.alex@gmail.com>
2025-09-28 03:50:44 -0300

    wip

--- internal/adapters/api/http/handlers/user_handlers.go
+++ internal/adapters/api/http/handlers/user_handlers.go
@@ -7,20 +7,21 @@ import (
 	"go-ecommerce/internal/core/domain"
 	"go-ecommerce/internal/core/ports"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 )
 
 type UserHandler struct {
-	us ports.UserService
+	srv ports.UserService
 }
 
 func NewUserHandler(us ports.UserService) *UserHandler {
-	return &UserHandler{us: us}
+	return &UserHandler{srv: us}
 }
 
-func (h *UserHandler) SaveUser(w http.ResponseWriter, r *http.Request) {
+func (uh *UserHandler) SaveUser(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Name     string `json:"name"`
 		Email    string `json:"email"`
@@ -81,11 +82,147 @@ func (h *UserHandler) SaveUser(w http.ResponseWriter, r *http.Request) {
 		Role:     role,
 	}
 
-	user, err := h.us.SaveUser(r.Context(), inputs)
+	user, err := uh.srv.SaveUser(r.Context(), inputs)
 	if err != nil {
 		httpdtos.RespondError(w, http.StatusBadRequest, fmt.Sprintf("Error creating user: %v", err))
 		return
 	}
 
 	httpdtos.RespondJSON(w, http.StatusCreated, "User successfully registered", user)
+}
+
+func (uh *UserHandler) FindUserById(r *http.Request, w http.ResponseWriter) {
+	if r.Method != http.MethodGet {
+		httpdtos.RespondError(w, http.StatusMethodNotAllowed, "method not allowed")
+		return
+	}
+
+	// find id from url param
+	userId := chi.URLParam(r, "user_id")
+	if userId == "" {
+		httpdtos.RespondError(w, http.StatusBadRequest, "userID is required")
+		return
+	}
+
+	// convert string id to uuid
+	parsedId, err := uuid.Parse(userId)
+	if err != nil {
+		httpdtos.RespondError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// find category calling service
+	user, err := uh.srv.GetUserByID(r.Context(), parsedId)
+	if err != nil {
+		if err == domain.ErrUserNotFound || user == nil {
+			httpdtos.RespondJSON(w, http.StatusNotFound, err.Error(), nil)
+			return
+		}
+
+		httpdtos.RespondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	httpdtos.RespondJSON(w, http.StatusOK, "user successfully retrieved", user)
+}
+
+func (uh *UserHandler) FindUserByEmail(r *http.Request, w http.ResponseWriter) {
+	if r.Method != http.MethodGet {
+		httpdtos.RespondError(w, http.StatusMethodNotAllowed, "method not allowed")
+		return
+	}
+
+	// find id from url param
+	userEmail := r.URL.Query().Get("email")
+	if userEmail == "" {
+		httpdtos.RespondError(w, http.StatusBadRequest, "user email is required")
+		return
+	}
+
+	// find category calling service
+	user, err := uh.srv.GetUserByEmail(r.Context(), userEmail)
+	if err != nil {
+		if err == domain.ErrUserNotFound || user == nil {
+			httpdtos.RespondJSON(w, http.StatusNotFound, err.Error(), nil)
+			return
+		}
+
+		httpdtos.RespondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	httpdtos.RespondJSON(w, http.StatusOK, "user successfully retrieved", user)
+}
+
+func (uh *UserHandler) ListUsers(r *http.Request, w http.ResponseWriter) {
+	if r.Method != http.MethodGet {
+		httpdtos.RespondError(w, http.StatusMethodNotAllowed, "method not allowed")
+		return
+	}
+
+	skipStr := r.URL.Query().Get("skip")
+	limitStr := r.URL.Query().Get("limit")
+
+	skip := 0
+	limit := 20
+
+	if limitStr != "" {
+		l, err := strconv.Atoi(limitStr)
+		if err == nil && l > 0 {
+			limit = l
+		}
+	}
+
+	if skipStr != "" {
+		s, err := strconv.Atoi(skipStr)
+		if err == nil && s >= 0 {
+			skip = s
+		}
+	}
+
+	// find all category calling service
+	users, err := uh.srv.ListUsers(r.Context(), uint64(skip), uint64(limit))
+	if err != nil {
+		if err == domain.ErrUserNotFound || len(users) <= 0 {
+			httpdtos.RespondError(w, http.StatusNotFound, err.Error())
+			return
+		}
+		httpdtos.RespondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	httpdtos.RespondJSON(w, http.StatusOK, "users successfully retrieved", users)
+}
+
+func (uh *UserHandler) DeleteUser(r *http.Request, w http.ResponseWriter) {
+	if r.Method != http.MethodDelete {
+		httpdtos.RespondError(w, http.StatusMethodNotAllowed, "method not allowed")
+		return
+	}
+
+	// find id from url param
+	userId := chi.URLParam(r, "user_id")
+	if userId == "" {
+		httpdtos.RespondError(w, http.StatusBadRequest, "userID is required to delete a product")
+		return
+	}
+
+	// parse string to uint
+	parsedId, err := uuid.Parse(userId)
+	if err != nil {
+		httpdtos.RespondError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	err = uh.srv.DeleteUser(r.Context(), parsedId)
+	if err != nil {
+		if err == domain.ErrUserNotFound {
+			httpdtos.RespondError(w, http.StatusNotFound, err.Error())
+			return
+		}
+		httpdtos.RespondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	httpdtos.RespondJSON(w, http.StatusOK, "user successfully deleted", nil)
 }
