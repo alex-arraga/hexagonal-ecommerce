e6b33dcfeba56d945b5ce9dc626a3cd16f651f22
Alex Arraga <arraga.alex@gmail.com>
2025-09-28 02:49:10 -0300

    wip

--- internal/adapters/api/http/routes/product_routes.go
+++ internal/adapters/api/http/routes/product_routes.go
@@ -9,8 +9,20 @@ import (
 
 func LoadProductRoutes(r chi.Router, h *handlers.ProductHandler) {
 	r.Route("/product", func(r chi.Router) {
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			h.ListProducts(r, w)
+		})
+		r.Get("/{product_id}", func(w http.ResponseWriter, r *http.Request) {
+			h.FindProductById(r, w)
+		})
 		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
 			h.SaveProduct(r, w)
+		})
+		r.Put("/{product_id}", func(w http.ResponseWriter, r *http.Request) {
+			h.SaveProduct(r, w)
+		})
+		r.Delete("/{product_id}", func(w http.ResponseWriter, r *http.Request) {
+			h.DeleteProduct(r, w)
 		})
 	})
 }
